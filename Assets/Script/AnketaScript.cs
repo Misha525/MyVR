e4b739a062aeba355f601af960b3da156f608fff
Misha_uniti2 <miha150908@gmail.com>
2024-11-13 19:47:10 +0300

    AnketaAktiv

--- Assets/Script/AnketaScript.cs
+++ Assets/Script/AnketaScript.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class AnketaScript : MonoBehaviour
 {
     public TMP_InputField nameImputFiled;
     public TMP_Text nameText;
-    // Start is called before the first frame update
+    
+    // public TMP_InputField ageImputFiled;
+    // public TMP_Text ageTmpText;
+
     public void OnButtonUserName()
     { 
         nameText.text = nameImputFiled.text; 
-    }
-
-    // Update is called once per frame
-   public void Update()
-    {
-        
     }
 }
